74a42305cfd510ecee026878882ad442aa4f364d
Reo Dongmin Lee <reoim10@gmail.com>
2023-10-03 17:57:27 +0900

    Update enable-api.py

--- enable-api.py
+++ enable-api.py
@@ -1,23 +1,43 @@
 import subprocess
 import json
+import argparse
 
-def get_project_ids(organization):
+def get_arguments():
+    """
+    Parsing the arugments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--organization', '-o', help='Example) --organization 302793038411', dest='org', required=True)
+    parser.add_argument('--folder', '-f', help='Example) --folder 112793038411', dest='folder')
+    parser.add_argument('--services', '-s', nargs='+', 
+                        help='Example) --services "policyanalyzer.googleapis.com" "bigquery.googleapis.com"', 
+                        default=[], dest='service', required=True)
+
+    org_id = parser.parse_args().org
+    folder_id = parser.parse_args().folder
+    services_list = parser.parse_args().service
+
+    return org_id, folder_id, services_list
+
+
+def get_project_ids(org_id, folder_id):
     """Extracts list of project IDs from the output of the gcloud command.
 
     Args:
-        output: Output of the gcloud command.
+        org_id: Orgnazation ID
+        folder_id: Folder ID
 
     Returns:
         List of project IDs.
     """
-    if folder == '':
+    if folder_id =='' or folder_id is None:
         command = ['gcloud', 'asset', 'search-all-resources', 
-                    '--asset-types='+'cloudresourcemanager.googleapis.com/Project', 
-                    '--scope=organizations/'+organization, '--format=json']
+                        '--asset-types='+'cloudresourcemanager.googleapis.com/Project', 
+                        '--scope=organizations/'+org_id, '--format=json']
     else:
         command = ['gcloud', 'asset', 'search-all-resources', 
                     '--asset-types='+'cloudresourcemanager.googleapis.com/Project', 
-                    '--scope=folders/'+folder, '--format=json']
+                    '--scope=folders/'+folder_id, '--format=json']
 
     output = subprocess.check_output(command)
     projects = json.loads(output)
@@ -27,36 +47,22 @@ def get_project_ids(organization):
         project_ids.append(project['additionalAttributes']['projectId'])
     return project_ids
 
-def enable_api(service, project_id):
-    """Runs a command for a given project ID.
+
+def enable_api(services_list, project_id):
+    """Enable services given project ID.
 
     Args:
         project_id: Project ID.
-        command: Command to run.
+        services_list: Service Names of API to be enabled on the project
     """
-    print(f'Enable API: {service} for project "{project_id}"')
-    subprocess.run(['gcloud', 'services', 'enable', service, '--project='+project_id])
+    for service in services_list:
+        print(f'Enable API: {service} for project "{project_id}"')
+        subprocess.run(['gcloud', 'services', 'enable', service, '--project='+project_id])
 
-          
-
-# Get the organization parameter.
-organization = input('Enter the organization id: ')
-if organization =='':
-    print('Please enter the organization id')
-    exit()
-
-# Get the folder parameter.
-print('\nBy default, this script will enalbe the API for all projects under the organization')
-folder = input('Enter the folder id if you want to enable the API only at the folder level: ')
-
-# Get the service parameter.
-print('\nService name example: policyanalyzer.googleapis.com')
-service = input('Enter the service name of the API that you want to enable: ')
-
+org_id, folder_id, services_list = get_arguments()
 
 # Extract the list of project IDs from the output.
-project_ids = get_project_ids(organization)
+project_ids = get_project_ids(org_id, folder_id)
 
-# Run the command for each project ID.
 for project_id in project_ids:
-  enable_api(service, project_id)
+  enable_api(services_list, project_id)
